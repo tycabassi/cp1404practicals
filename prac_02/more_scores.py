50b153d09c2a86c1a1d96161759b4cdcf4214407
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-27 09:17:44 +1000

    Update code to send scores to file

--- prac_02/more_scores.py
+++ prac_02/more_scores.py
@@ -1,8 +1,25 @@
 import random
 
+
 def main():
+    number_of_scores = int(input("How many scores do you want to generate? "))
+    in_file = open("results.txt", "w")
+    for i in range(number_of_scores):
+        random_score = random.randint(1, 100)
+        score = determine_score(random_score)
+        in_file.write(f"{random_score} is {score}\n")
+    in_file.close()
 
 
+def determine_score(random_score):
+    if random_score < 0 or random_score > 100:
+        return "Invalid score"
+    elif random_score >= 90:
+        return "Excellent"
+    elif random_score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
 
 
 main()
