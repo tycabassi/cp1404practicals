ed54988045f7c44ee2f81c00cd1329ad478d2384
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-26 13:58:59 +1000

    Added the random score to the main and determined the score of it

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,12 +2,16 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
 
 def main():
     score = float(input("Enter score: "))
     grade = determine_score(score)
-    print(grade)
+    print(f"The user's score is {score} which is {grade}")
+    random_score = random.randint(1, 100)
+    random_grade = determine_score(random_score)
+    print(f"The random's score is {random_score} which is {random_grade}")
 
 
 def determine_score(score):
