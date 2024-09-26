e661cff5144e11a61a0d4495cb822e2f083a7ba5
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-26 13:33:46 +1000

    Refractor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -4,11 +4,13 @@ def main():
 
 
 def print_password(password):
+    """Print stars based on the length of the password"""
     for i in range(len(password)):
         print("*", end="")
 
 
 def get_password():
+    """Get password ensuring its valid"""
     password = input("Enter a password: ")
     while len(password) < 10:
         print("Invalid enter password again!")
