e0362172c1bd7aa8fc40f01dd1dae530823a0fdc
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-26 13:12:50 +1000

    Add loops.py to prac_01

---
+++ prac_01/loops.py
@@ -0,0 +1,30 @@
+# for i in range(1, 21, 2):
+#     print(i, end=' ')
+# print()
+
+# a
+for i in range(0, 101, 10):
+    print(i, end=' ')
+print()
+
+# b
+for i in range(20, 0, -1):
+    print(i, end=' ')
+print()
+
+
+
+# c
+number_of_stars = int(input("Enter number of stars: "))
+for i in range(number_of_stars):
+    print("*", end='')
+
+
+
+
+# d
+count = 0
+number_of_stars = int(input("Enter number of stars: "))
+for i in range(number_of_stars):
+    count += 1
+    print("*" * count)
