9d6a61c333aa7520d6d051486b03cb5135c9e9a9
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-26 14:28:21 +1000

    Refactored fahrenheit to celsius into a function

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -8,14 +8,23 @@ def main():
     if choice == "C":
         convert_celsius_to_fahrenheit()
     elif choice == "F":
-        fahrenheit = float(input("Fahrenheit: "))
-        celsius = 5 / 9 * (fahrenheit - 32)
-        print(f"Result: {celsius:.2f} C")
+        convert_fahrenheit_to_celsius()
     else:
         print("Invalid choice")
+
+
+def convert_fahrenheit_to_celsius():
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = 5 / 9 * (fahrenheit - 32)
+    print(f"Result: {celsius:.2f} C")
 
 
 def convert_celsius_to_fahrenheit():
     celsius = float(input("Celsius: "))
     fahrenheit = celsius * 9.0 / 5 + 32
     print(f"Result: {fahrenheit:.2f} F")
+
+
+
+
+main()
