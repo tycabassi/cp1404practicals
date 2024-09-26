985ec650b3527447ac317631bce19cdfb54c0038
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-26 13:32:42 +1000

    Refractor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,6 +1,19 @@
-password = input("Enter a password: ")
-while len(password) < 10:
-    print("Invalid enter password again!")
+def main():
+    password = get_password()
+    print_password(password)
+
+
+def print_password(password):
+    for i in range(len(password)):
+        print("*", end="")
+
+
+def get_password():
     password = input("Enter a password: ")
-for i in range(len(password)):
-    print("*", end="")
+    while len(password) < 10:
+        print("Invalid enter password again!")
+        password = input("Enter a password: ")
+    return password
+
+
+main()
