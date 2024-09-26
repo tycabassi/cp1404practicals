dee5137f75384a2c2e3e3f67734e6773dae10551
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-26 14:18:01 +1000

    Update the menu to include 2/3 functions

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -4,21 +4,40 @@ MENU = """(G)et a valid score(must be 0-100 inclusive)
 (Q)uit"""
 
 
-
 def main():
+    score = get_valid_score()
     print(MENU)
     choice = input(">>>: ").upper()
     while choice != "Q":
         if choice == "G":
-            pass
+            score = get_valid_score()
         elif choice == "P":
-            pass
+            determine_score(score)
         elif choice == "S":
             pass
         else:
             print("Invalid choice")
         print(MENU)
         choice = input(">>>: ").upper()
+
+
+def get_valid_score():
+    score = int(input("Enter a score between 1 and 100: "))
+    while score < 1 or score > 100:
+        print("Invalid")
+        score = int(input("Enter a score between 1 and 100: "))
+    return score
+
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
 
 
 main()
