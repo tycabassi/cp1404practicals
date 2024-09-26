cdb1cca888be3401b2ea5dd5cd657121475da46f
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-26 13:51:37 +1000

    Refactored determining the score into a function

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,12 +2,23 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+
+
+def main():
+    score = float(input("Enter score: "))
+    grade = determine_score(score)
+    print(grade)
+
+
+def determine_score(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
