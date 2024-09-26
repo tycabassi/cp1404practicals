1c9c3fd21574a55951f0fbf072581e3a28aa43f8
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-26 13:13:53 +1000

    Add menus.py to prac_01

---
+++ prac_01/menus.py
@@ -0,0 +1,29 @@
+"""get name
+display menu
+get choice
+while choice != Q
+   if choice == H
+       display "hello" name
+   else if choice == G
+       display "goodbye" name
+   else
+       display invalid message
+   display menu
+   get choice
+display finished message
+"""
+MENU = "(H)ello\n(G)oodbye\n(Q)uit"
+
+name = input("Enter name: ")
+print(MENU)
+choice = input(">>>").upper()
+while choice != "Q":
+    if choice == "H":
+        print(f"Hello {name}")
+    elif choice == "G":
+        print(f"Goodbye {name}")
+    else:
+        print("Invalid choice")
+    print(MENU)
+    choice = input(">>>").upper()
+print("Finished")
