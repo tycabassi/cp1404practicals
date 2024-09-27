aa2c9f38d944dfebb419347a28669768842a3fb0
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-27 12:17:43 +1000

    Add the section which sends output to the file

--- prac_02/convert_temps.py
+++ prac_02/convert_temps.py
@@ -2,11 +2,24 @@ import random
 
 
 def main():
+
+
+
+def get_temperature_fahrenheit():
     in_file = open('temps_input.txt', 'w')
     for i in range(20):
         random_temperature = random.uniform(-200, 200)
         in_file.write(f"{random_temperature} degrees fahrenheit\n")
     in_file.close()
+
+
+def calculate_temperature_celsius(fahrenheit):
+    in_file = open('temps_input.txt', 'r')
+    out_file = open('temps_output.txt', 'w')
+    celsius = (fahrenheit - 32) * 5 / 9
+    in_file.write(f"{fahrenheit}degrees fahrenheit = {celsius}\n")
+    in_file.close()
+    out_file.close()
 
 
 main()
