df10ff8870fee631724932b58af8269c5b60d0a5
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-29 11:45:13 +1000

    Add born % and death % to function

--- prac_02/gopher_population.py
+++ prac_02/gopher_population.py
@@ -6,10 +6,18 @@ def main():
     for i in range(YEARS):
         while i < YEARS:
             population = int(input("Starting Population: "))
+            calculate_population(population)
             i += 1
             print(f"Year: {i}")
             print(f"Population: {population}")
 
+
+
+def calculate_population(population):
+    born_rate = random.randint(10,20)
+    death_rate = random.randint(5, 25)
+    print(f"Born rate: {born_rate}")
+    print(f"Deathrate: {death_rate}")
 
 
 
