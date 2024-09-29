b0b46e1edfa7005437d0339790153b38915a82f7
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-29 11:28:53 +1000

    Break up 2 functions into main and 1 function

--- prac_02/convert_temps.py
+++ prac_02/convert_temps.py
@@ -2,27 +2,31 @@ import random
 
 
 def main():
-    fahrenheit = get_temperature_fahrenheit()
-    # Returns none first function works fine, troubleshoot after work
-    calculate_temperature_celsius(fahrenheit)
-
-
-
-def get_temperature_fahrenheit():
-    in_file = open('temps_input.txt', 'w')
-    for i in range(20):
-        random_temperature = random.uniform(-200, 200)
-        in_file.write(f"{random_temperature} degrees fahrenheit\n")
-    in_file.close()
-
-
-def calculate_temperature_celsius(fahrenheit):
+    create_input_file(15)
     in_file = open('temps_input.txt', 'r')
     out_file = open('temps_output.txt', 'w')
-    celsius = (fahrenheit - 32) * 5 / 9
-    in_file.write(f"{fahrenheit}degrees fahrenheit = {celsius}\n")
+    for line in in_file:
+        result = convert_fahrenheit_to_celsius(float(line))
+        print(result, file=out_file)
     in_file.close()
     out_file.close()
+
+
+def create_input_file(quantity):
+    """Write number (quantity) of temperatures to file."""
+    temperatures_file = open("temps_input.txt", "w")
+    for i in range(quantity):
+        temperature = random.uniform(-200, 200)
+        print(temperature, file=temperatures_file)
+    temperatures_file.close()
+
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
+
+
+def convert_celsius_to_fahrenheit(celsius):
+    return (celsius * 9 / 5) + 32
 
 
 main()
