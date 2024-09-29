28bd608e4efaada352a6d97c82464c5815fc3073
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-09-29 11:40:49 +1000

    Update main to have the basic functionality of adding years

---
+++ prac_02/gopher_population.py
@@ -0,0 +1,25 @@
+
+import random
+YEARS = 10
+MENU = "Welcome to the starting Gopher Population Simulator"
+def main():
+    for i in range(YEARS):
+        while i < YEARS:
+            population = int(input("Starting Population: "))
+            i += 1
+            print(f"Year: {i}")
+            print(f"Population: {population}")
+
+
+
+
+
+
+# def gopher_deaths_calculator(starting_population):
+
+
+
+
+
+
+main()
