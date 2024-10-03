ed2ab2c1045a19140d74717400e63f26b17102c0
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-03 11:30:18 +1000

    Add the functionality to count the days

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,6 +9,7 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
+days = 0
 MAX_INCREASE = 0.1  # 10%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01
@@ -16,7 +17,7 @@ MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
 
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
+print(f"Starting price: ${price:,.2f}")
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -30,6 +31,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
         # generate a random floating-point number
         # between negative MAX_DECREASE and 0
         price_change = random.uniform(-MAX_DECREASE, 0)
-
+    days += 1
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {days} the price is ${price:,.2f}")
