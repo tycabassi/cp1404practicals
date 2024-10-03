e6c1fc0908937b09a0c6bcd08d1bcbd5d9384dd1
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-03 13:30:17 +1000

    Answer the questions given based on the sample code

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -7,7 +7,12 @@ is_finished = False
 while not is_finished:
     try:
         result = int(input("Enter a valid integer: "))
-        # TODO: this line
-    except:  # TODO - add the exception you want to catch after except
+        is_finished = True
+    except ValueError:  # TODO - add the exception you want to catch after except
         print("Please enter a valid integer.")
+        result = int(input("Enter a valid integer: "))
 print("Valid result is:", result)
+
+# A ValueError would occur if i was to type in say "w" or a symbol instead of an integer
+# A ZeroDivisionError wouldn't occur unless we try to divide a number by result variable when the result = 0
+# To avoid a ZeroDivisionError an error checking of the result value will be needed the same as for the ValueError
