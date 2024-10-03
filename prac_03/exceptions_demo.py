fa17199a236d0597bbc08ae3e724e8607fabbbb5
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-03 13:37:59 +1000

    Update code to handle ZeroDivision error

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -6,11 +6,15 @@ Fill in the TODOs to complete the task
 is_finished = False
 while not is_finished:
     try:
-        result = int(input("Enter a valid integer: "))
+        numerator = int(input("Enter a valid integer: "))
+        denominator = int(input("Enter a 2nd integer: "))
+        division = numerator / denominator
         is_finished = True
     except ValueError:
         print("Please enter a valid integer.")
-print("Valid result is:", result)
+    except ZeroDivisionError:
+        print("Please enter a number which isnt divisible by 0")
+print("Division result is:", division)
 
 # A ValueError would occur if i was to type in say "w" or a symbol instead of an integer
 # A ZeroDivisionError wouldn't occur unless we try to divide a number by result variable when the result = 0
