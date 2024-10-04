bab6798a2fd9edaf157271a075f984e5c08e6b74
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-04 11:17:59 +1000

    Update comments beside constants to fit new values

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -10,7 +10,7 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 import random
 
 days = 0
-MAX_INCREASE = 0.175  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01
 MAX_PRICE = 100
