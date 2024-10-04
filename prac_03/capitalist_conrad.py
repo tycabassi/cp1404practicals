7ea89ec871ea30f7b55d0315d9efbd52d1aa6fe9
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-04 11:22:35 +1000

    Move days variable to correct position

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -9,13 +9,14 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-days = 0
+
 MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 0.01
 MAX_PRICE = 100
 INITIAL_PRICE = 1.0
 FILENAME = "capitalist_conrad.txt"
+days = 0
 out_file = open(FILENAME, "w")
 price = INITIAL_PRICE
 print(f"Starting price: ${price:,.2f}", file=out_file)
