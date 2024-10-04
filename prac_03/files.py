cc1bac8024f0fb4c49244aad5b999aea4438434a
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-04 11:14:50 +1000

    Uncomment answers

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,5 +1,27 @@
-#1.
+# 1.
 name = input("Enter your name: ")
 out_file = open(f"{name}.txt", "w")
 out_file.write(name)
 out_file.close()
+
+
+# 2.
+name = input("Enter your name: ")
+in_file = open(f"{name}.txt", "r")
+print(f"Hello, {name}!")
+in_file.close()
+
+# 3.
+in_file = open("numbers.txt", "r")
+number1 = int(in_file.readline())
+number2 = int(in_file.readline())
+print(f"Sum of the first two lines is {number1 + number2}")
+in_file.close()
+
+sum_of_numbers = 0
+in_file = open("numbers.txt", "r")
+for line in in_file:
+    number = int(line)
+    sum_of_numbers += number
+print(f"Sum of the numbers is {sum_of_numbers}")
+in_file.close()
