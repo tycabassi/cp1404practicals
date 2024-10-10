116dfcc3071cf6555f66923aa90a8b8739372d84
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-10 13:51:12 +1000

    Update load_data function to return a Nested Loop

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -14,16 +14,32 @@ def main():
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subject_records = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        # print("----------")
+        subject_records.append(parts)
     input_file.close()
+    return subject_records
+
+
+# def load_data():
+#     """Read data from file formatted like: subject,lecturer,number of students."""
+#     input_file = open(FILENAME)
+#     for line in input_file:
+#         print(line)  # See what a line looks like
+#         print(repr(line))  # See what a line really looks like
+#         line = line.strip()  # Remove the \n
+#         parts = line.split(',')  # Separate the data into its parts
+#         print(parts)  # See what the parts look like (notice the integer is a string)
+#         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+#         print(parts)  # See if that worked
+#         print("----------")
+#     input_file.close()
 
 
 main()
