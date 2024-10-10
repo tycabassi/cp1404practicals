f2b75c9ac1249bccf4c3d14004584e8c41990888
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-10 14:22:27 +1000

    Add the 2nd list_exercises.py code

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,5 +1,5 @@
+# 1. Basic List Operations
 numbers = []
-
 
 for i in range(5):
     number = int(input("Enter a number: "))
@@ -10,3 +10,14 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of numbers is {sum(numbers)/len(numbers)}")
+
+
+#2.Woefully inadeuqate security checker
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input('Enter a username:')
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied!")
