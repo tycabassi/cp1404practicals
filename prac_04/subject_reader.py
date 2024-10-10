3461a7f8275ede4d449f5b8499143cf49da13003
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-10 14:09:29 +1000

    Update the code to have a display_subject_details function which formats and displays the list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,24 +7,28 @@ FILENAME = "subject_data.txt"
 
 
 def main():
-    data = load_data()
-    print(data)
+    subjects = load_data()
+    # print(subjects)
+    display_subject_details(subjects)
 
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
-    subject_records = []
+    subjects = []
     for line in input_file:
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        # print(parts)  # See if that worked
-        # print("----------")
-        subject_records.append(parts)
+        subjects.append(parts)
     input_file.close()
-    return subject_records
+    return subjects
+
+
+def display_subject_details(subjects):
+    """Display subject, lecturer, number of students"""
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
 
 
 # def load_data():
