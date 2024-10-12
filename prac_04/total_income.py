00f1709bf3d18c284a12d48c773d761e876fecd8
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-12 16:38:11 +1000

    Format the file getting rid of pycharms warning

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -5,18 +5,17 @@ Starter code for cumulative total income program
 
 
 def main():
-    """Display income report for incomes over a given number of months."""
+    """Display income report for incomes over a given number of months"""
     incomes = []
     number_of_months = int(input("How many months? "))
-
     for month in range(1, number_of_months + 1):  # 1 as it starts at month 1
         income = float(input(f"Enter income for month {month}: "))
         incomes.append(income)
-
     print_report(incomes)
 
 
 def print_report(incomes):
+    """Display report based on income from user input"""
     print("\nIncome Report\n-------------")
     total = 0
     for month, income in enumerate(incomes, 1):
