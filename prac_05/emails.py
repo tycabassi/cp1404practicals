e07c40d3b7ea2ea2f0ec6b9e410ec4bcbe29cd7d
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-17 16:38:33 +1000

    Add a function to format the plausible name from the email

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,6 +1,23 @@
-emails_to_names = {}
+def main():
+    emails_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        likely_name = get_plausible_name_from_email(email)
+        name_check = input(f"Is your name {likely_name} y/n? ").lower()
+        if name_check != "y" and name_check != "":
+            name = input("Name: ")
+        emails_to_name[email] = name
+        email = input("Email: ")
+    for email, name in emails_to_name.items():
+        print(name, email)
 
-email = input("Email: ")
-choice = input(f"Is your name {email.split("@")[0]} y/n? ")
 
-# tycab@bigpond.com
+def get_plausible_name_from_email(email):
+    """ Returns the most likely name from the email address"""
+    value_before_address = email.split("@")[0]
+    parts = value_before_address.split('.')
+    name = " ".join(parts).title()
+    return name
+
+
+main()
