e26111bef77b1214468c9c86b6154334416d449c
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-31 14:17:36 +1000

    Add get_age function to guitar.py

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,6 +3,8 @@
 # cost = 16035.40
 # print(f"My guitar: {name}, first made in {year}")
 
+CURRENT_YEAR = 2024
+
 
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
@@ -13,6 +15,10 @@ class Guitar:
     def __str__(self):
         return f"{self.name} ({self.year}) : ${self.cost}"
 
+    def get_age(self):
+        return CURRENT_YEAR - self.year
+
 
 guitar_details = Guitar("Gibson L-5 CES", 1922, 16035.40)
 print(guitar_details)
+print(guitar_details.get_age())
