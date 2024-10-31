d7819aca600a017359c6bd513b1ca732252d0dbd
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-10-31 22:02:14 +1000

    Extract Guitar class from guitar.py

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,5 +1,14 @@
+from guitar import Guitar
 my_guitars = []
 print("My Guitars!")
 guitar_name = input("Name: ")
 year = input("Year: ")
 cost = float(input("Cost: $"))
+
+new_guitar = Guitar(guitar_name, year, cost)
+print(new_guitar)
+
+
+
+
+
