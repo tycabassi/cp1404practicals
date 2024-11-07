c931497e2bf3a714e85d80b2f069e4a4330f7cfc
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-11-07 13:34:21 +1000

    Fix up code in both files and csv to include pointer arithemtic values, ensuring the code doesn't crash

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -28,7 +28,7 @@ def main():
         reflection = parts[2] == "Yes"
         # Construct a ProgrammingLanguage object using the elements
         # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
+        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]),parts[4])
         # Add the language we've just constructed to the list
         languages.append(language)
     # Close the file as soon as we've finished reading it
