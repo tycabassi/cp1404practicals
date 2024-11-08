ba3497fdd23b18b33fd684ae1e1aa974d5a24a42
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-11-08 14:15:39 +1000

    Separate the code into functions making it more legible

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,11 +2,20 @@ from prac_07.guitar import Guitar
 
 
 def main():
-    in_file = open("guitars.csv")
-    for line in in_file:
-        parts = line.strip().split(",")
-        guitar = Guitar(parts[0], parts[1], parts[2])
+    guitars = []
+    get_guitar_details(guitars)
+    guitars.sort() # Sorts using __lt__ method used in guitar.py
+    for guitar in guitars:
         print(guitar)
+
+
+def get_guitar_details(guitars):
+    with open("guitars.csv") as in_file:
+        for line in in_file:
+            parts = line.strip().split(",")
+            guitar = Guitar(parts[0], parts[1], parts[2])
+            guitars.append(guitar)
+    return guitars
 
 
 main()
