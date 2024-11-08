b4c28475c57ae92df25eb45b9146412ed3647a46
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-11-08 14:34:06 +1000

    Add function to write each guitar to the guitars.csv file

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -13,21 +13,28 @@ def main():
         guitar_name = input("Name: ")
         guitars.append(new_guitar)
     sort_guitars(guitars)
+    write_guitars_to_file(guitars)
 
 
 def get_guitar_details(guitars):
-    with open("guitars.csv") as in_file:
+    with open('guitars.csv', 'r') as in_file:
         for line in in_file:
             parts = line.strip().split(",")
             guitar = Guitar(parts[0], parts[1], parts[2])
             guitars.append(guitar)
-    return guitars
+        return guitars
 
 
 def sort_guitars(guitars):
     guitars.sort()  # Sorts using __lt__ method used in guitar.py
     for guitar in guitars:
         print(guitar)
+
+
+def write_guitars_to_file(guitars):
+    with open('guitars.csv', 'w') as in_file:
+        for guitar in guitars:
+            in_file.write(str(f"{guitar}\n"))
 
 
 main()
