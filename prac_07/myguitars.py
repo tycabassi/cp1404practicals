b6b23eaba1e7137cafa9f19b04e82e735a74481f
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-11-08 14:19:24 +1000

    Add code from prac_06 to myguitars.py

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -4,9 +4,13 @@ from prac_07.guitar import Guitar
 def main():
     guitars = []
     get_guitar_details(guitars)
-    guitars.sort() # Sorts using __lt__ method used in guitar.py
-    for guitar in guitars:
-        print(guitar)
+    guitar_name = input("Name: ")
+    while guitar_name != "":
+        year = int(input("Year: "))  # Needs to be and int as its subtracted from CURRENT_YEAR in is_vintage()
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(guitar_name, year, cost)
+        print(f"{new_guitar} added")
+        guitar_name = input("Name: ")
 
 
 def get_guitar_details(guitars):
@@ -16,6 +20,12 @@ def get_guitar_details(guitars):
             guitar = Guitar(parts[0], parts[1], parts[2])
             guitars.append(guitar)
     return guitars
+
+
+def sort_guitars(guitars):
+    guitars.sort()  # Sorts using __lt__ method used in guitar.py
+    for guitar in guitars:
+        print(guitar)
 
 
 main()
