da9d4cfc66bcb0d2f1af39df317f6a36af1a11e0
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-11-20 13:47:31 +1000

    Update code to answer the last two questions + formatting check

--- prac_09/test_taxi.py
+++ prac_09/test_taxi.py
@@ -3,3 +3,6 @@ from prac_09.taxi import Taxi
 my_taxi = Taxi("Prius 1", 100, 1.23)
 my_taxi.drive(40)
 print(f"{my_taxi.fuel}, ${my_taxi.get_fare()}")
+my_taxi.start_fare()
+my_taxi.drive(100)
+print(f"Distance travelled {my_taxi.odometer}km  Fare: ${my_taxi.get_fare()}")
