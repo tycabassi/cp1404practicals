7e49b2546b859f911b071e0240271e104d8a81ca
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-11-24 09:16:25 +1000

    Display taxi choices and ensure you cant drive if you haven't chose a vehicle

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -8,11 +8,19 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 def main():
     print(INITIAL_MESSAGE)
     print(MENU)
+    current_taxi = None
     choice = input(">>>")
     while choice != "q":
         if choice == "c":
+            print("Taxis available")
+            print(Taxi("Prius",100))
+            print(SilverServiceTaxi("Limo", 100,2))
+            print(SilverServiceTaxi("Hummer",200,4))
             pass
+            current_taxi = True
         elif choice == "d":
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
             pass
         else:
             print("Invalid choice")
