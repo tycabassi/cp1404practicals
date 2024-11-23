11b7c0a29540fc190dcd93a4dda321218f71d61a
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-11-23 18:53:06 +1000

    Add tests to silver_service_taxi_test.py

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -3,14 +3,18 @@ from taxi import Taxi
 
 class SilverServiceTaxi(Taxi):
     """Represents the High-rollers Silver Service Taxi"""
-    flag_fall = 4.50
+    flag_fall = 4.5
 
     def __init__(self, name, fuel, fanciness):
         """Initialises the Silver Service Taxi class"""
         super().__init__(name, fuel)
         self.fanciness = fanciness
-        self.price_per_km += fanciness
+        self.price_per_km *= fanciness
 
     def __str__(self):
         """Returns a string representation of the Silver Service Taxi"""
         return f"{super().__str__()} + flag fall of ${self.flag_fall:.2f}"
+
+    def calculate_fare(self):
+        """Calculates the current fare"""
+        return self.flag_fall + super().get_fare()
---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,6 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+taxi = SilverServiceTaxi("Premium Ride", 100, 2)
+taxi.drive(18)
+print(taxi)
+print(taxi.calculate_fare())
