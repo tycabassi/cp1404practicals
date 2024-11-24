f6a725edade9668f12084f6062d0b1eee658bc3a
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-11-24 10:25:07 +1000

    Add docstrings to all functions

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -6,11 +6,12 @@ MENU = "q)uit, c)hoose taxi, d)rive"
 
 
 def main():
+    """Simulate a taxi service"""
     print(INITIAL_MESSAGE)
     print(MENU)
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
     current_taxi = None
-    bill_to_date = 0
+    total_fee = 0
     choice = input(">>>")
     while choice != "q":
         if choice == "c":
@@ -24,22 +25,25 @@ def main():
                 current_taxi.start_fare()
                 current_taxi.drive(distance)
                 print(f"The {current_taxi} trip cost you ${current_taxi.get_fare()}")
-                bill_to_date += current_taxi.get_fare()
-                print(f"Bill to date: ${bill_to_date}")
+                total_fee += current_taxi.get_fare()
+                print(f"Bill to date: ${total_fee}")
         else:
             print("Invalid choice")
         print(INITIAL_MESSAGE)
         print(MENU)
         choice = input(">>>")
+    print(f"Total bill: ${total_fee}")
 
 
 def display_taxis(taxis):
+    """Displays all the taxis"""
     print("Taxis available")
     for i, car in enumerate(taxis):
         print(f"{i} - {car}")
 
 
 def get_taxi_choice(taxis):
+    """Gets a taxi choice from the user and returns it"""
     choice_of_taxi = int(input("Choose Taxi: "))
     current_taxi = taxis[choice_of_taxi]
     return current_taxi
