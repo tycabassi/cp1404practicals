51c7fda5f2ca409e6708283b4374848d1705a385
tycabassi <ty.cabassi@my.jcu.edu.au>
2024-12-01 10:02:48 +1000

    Add error checking to wiki.py

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -2,7 +2,12 @@ import wikipedia
 
 user_search = input("Enter a Search phrase or Page title: ")
 while user_search != "":
-    page_details = wikipedia.search(user_search)
-    print(page_details)
+    try:
+        page_details = wikipedia.summary(user_search)
+        print(page_details)
+    except wikipedia.exceptions.DisambiguationError as e:
+        print(e.options)
+    except wikipedia.exceptions.PageError as e:
+        print(e)
     user_search = input("Enter a Search phrase or Page title: ")
-print("Thanks for using our cool Python search")
+print("Thanks for using our cool Wikipedia search")
